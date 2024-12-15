1c5d57b7297b070164c100c04829950c476230bd
Amir <hasanli.amir@gmail.com>
2024-12-15 21:35:01 +0100

    refactor: move OpenDocument function to IOHelper class
    Signed-off-by: Amir <hasanli.amir@gmail.com>

---
+++ Utils/IOHelper.cs
@@ -0,0 +1,21 @@
+﻿using Grasshopper.Kernel;
+using System;
+
+namespace Tenrec.Utils
+{
+    public static class IOHelper
+    {
+        public static bool OpenDocument(string filePath, out GH_Document doc)
+        {
+            var io = new GH_DocumentIO();
+            if (!io.Open(filePath))
+            {
+                doc = null;
+                throw new Exception($"Failed to open file: {filePath}");
+            }
+            doc = io.Document;
+            doc.Enabled = true;
+            return true;
+        }
+    }
+}

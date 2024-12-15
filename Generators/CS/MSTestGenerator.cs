de28595f96bc960042d95489000a2c1fce4f842d
Amir <hasanli.amir@gmail.com>
2024-12-15 21:34:12 +0100

    refactor: move and rename old generator.cs to MSTestGenerator.cs
    Signed-off-by: Amir <hasanli.amir@gmail.com>

--- Generators/CS/MSTestGenerator.cs
+++ Generators/CS/MSTestGenerator.cs
@@ -6,28 +6,12 @@ using System.Linq;
 using System.Text;
 using Tenrec.Components;
 
-namespace Tenrec
+namespace Tenrec.Generators.CS
 {
-    /// <summary>
-    /// Produces code files automatically, generating test code based on Grasshopper files.
-    /// </summary>
-    /// <remarks> 
-    /// This must be called once to produce the test files automatically, or every time you add or remove a <see cref="Group_UnitTest"/> or add or remove a file from the test folder.
-    /// </remarks>
-    public class Generator
+    public class MSTestGenerator : IGenerator
     {
-        /// <summary>
-        /// Generate a code file containing all the <see cref="Group_UnitTest"/>-based tests present in Grasshopper files.
-        /// </summary>
-        /// <param name="ghTestFolders">The folder containing the Grasshopper files.</param>
-        /// <param name="outputFolder">The folder where to save the source code file.</param>
-        /// <param name="outputName">The name of the resulting code file.</param>
-        /// <param name="language">Language of the code file. Currently only supports "cs"(C#).</param>
-        /// <param name="testFramework">Testing framework. Currently only supports MSTest.</param>
-        /// <returns>A log of the process.</returns>
-        public static string CreateAutoTestSourceFile(string[] ghTestFolders,
-            string outputFolder, string outputName = "TenrecGeneratedTests",
-            string language = "cs", string testFramework = "mstest")
+        public string CreateAutoTestSourceFile
+            (string[] ghTestFolders, string outputFolder, string outputName)
         {
             var log = new StringBuilder();
             var sb = new StringBuilder();
@@ -35,14 +19,10 @@ namespace Tenrec
             var fileName = string.Empty;
             try
             {
-                if (ghTestFolders == null && ghTestFolders.Length == 0)
+                if (ghTestFolders == null || ghTestFolders.Length == 0)
                     throw new ArgumentNullException(nameof(outputFolder));
                 if (string.IsNullOrEmpty(outputFolder))
                     throw new ArgumentNullException(nameof(outputFolder));
-                if (!language.Equals("cs"))
-                    throw new NotImplementedException(nameof(language));
-                if (!testFramework.Equals("mstest"))
-                    throw new NotImplementedException(nameof(testFramework));
 
                 sb.AppendLine("using Microsoft.VisualStudio.TestTools.UnitTesting;");
                 sb.AppendLine();
@@ -55,7 +35,7 @@ namespace Tenrec
                     {
                         foreach (var file in files)
                         {
-                            if (OpenDocument(file, out GH_Document doc))
+                            if (Utils.IOHelper.OpenDocument(file, out GH_Document doc))
                             {
                                 var groups = new List<IGH_DocumentObject>();
                                 foreach (var obj in doc.Objects)
@@ -65,10 +45,10 @@ namespace Tenrec
                                         groups.Add(obj);
                                     }
                                 }
-                                if (groups != null && groups.Any())
+                                if (groups.Any())
                                 {
                                     sb.AppendLine("    [TestClass]");
-                                    sb.AppendLine($"    public class AutoTest_{CodeableNickname(doc.DisplayName)}");
+                                    sb.AppendLine($"    public class AutoTest_{Utils.StringHelper.CodeableNickname(doc.DisplayName)}");
                                     sb.AppendLine("    {");
                                     sb.AppendLine($"        public string FilePath => @\"{doc.FilePath}\";");
                                     sb.AppendLine("        private TestContext testContextInstance;");
@@ -76,7 +56,7 @@ namespace Tenrec
                                     foreach (var group in groups)
                                     {
                                         sb.AppendLine("        [TestMethod]");
-                                        sb.AppendLine($"        public void {CodeableNickname(group.NickName)}()");
+                                        sb.AppendLine($"        public void {Utils.StringHelper.CodeableNickname(group.NickName)}()");
                                         sb.AppendLine("        {");
                                         sb.AppendLine($"            Tenrec.Runner.Initialize(TestContext);");
                                         sb.AppendLine($"            Tenrec.Runner.RunTenrecGroup(FilePath, new System.Guid(\"{group.InstanceGuid}\"), TestContext);");
@@ -110,24 +90,6 @@ namespace Tenrec
                 log.AppendLine($"File successfully created.");
 
             return log.ToString();
-        }
-
-        private static string CodeableNickname(string nickname)
-        {
-            return nickname.Replace(" ", "_");
-        }
-
-        private static bool OpenDocument(string filePath, out GH_Document doc)
-        {
-            var io = new GH_DocumentIO();
-            if (!io.Open(filePath))
-            {
-                doc = null;
-                throw new Exception($"Failed to open file: {filePath}");
-            }
-            doc = io.Document;
-            doc.Enabled = true;
-            return true;
         }
     }
 }

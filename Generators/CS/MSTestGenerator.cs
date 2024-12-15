e5b69def2317471a20ce00773e8518a5ff2fe2e6
Amir <hasanli.amir@gmail.com>
2024-12-15 22:53:21 +0100

    doc: remove duplicate documentation
    Signed-off-by: Amir <hasanli.amir@gmail.com>

--- Generators/CS/MSTestGenerator.cs
+++ Generators/CS/MSTestGenerator.cs
@@ -8,21 +8,10 @@ using Tenrec.Components;
 
 namespace Tenrec.Generators.CS
 {
-    /// <summary>
-    /// Produces code files automatically, generating test code based on Grasshopper files.
-    /// </summary>
-    /// <remarks> 
-    /// This must be called once to produce the test files automatically, or every time you add or remove a <see cref="Group_UnitTest"/> or add or remove a file from the test folder.
-    /// </remarks>
+    /// <inheritdoc/>
     public class MSTestGenerator : IGenerator
     {
-        /// <summary>
-        /// Generate a code file containing all the <see cref="Group_UnitTest"/>-based tests present in Grasshopper files.
-        /// </summary>
-        /// <param name="ghTestFolders">The folder containing the Grasshopper files.</param>
-        /// <param name="outputFolder">The folder where to save the source code file.</param>
-        /// <param name="outputName">The name of the resulting code file.</param>
-        /// <returns>A log of the process.</returns>
+        /// <inheritdoc/>
         public string CreateAutoTestSourceFile
             (string[] ghTestFolders, string outputFolder, string outputName)
         {

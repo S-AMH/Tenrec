758fa145d1572ee6f2bc46f728a11870cb70ee5c
Amir <hasanli.amir@gmail.com>
2024-12-15 21:35:46 +0100

    refactor: move CodeableNickName function to StringHelper
    Signed-off-by: Amir <hasanli.amir@gmail.com>

---
+++ Utils/StringHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Tenrec.Utils
+{
+    public static class StringHelper
+    {
+        public static string CodeableNickname(string nickname)
+        {
+            return nickname.Replace(" ", "_");
+        }
+        public static string IndexedString(string str, int index)
+        {
+            return string.Concat(System.Linq.Enumerable.Repeat("    ", index)) + str;
+        }
+    }
+}

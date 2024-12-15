d3a54e29812258625e68cdf9f7b6e16a5332f7e1
Amir <hasanli.amir@gmail.com>
2024-12-15 22:50:03 +0100

    doc: Update doc
    Signed-off-by: Amir <hasanli.amir@gmail.com>

--- Generators/CS/XUnitGenerator.cs
+++ Generators/CS/XUnitGenerator.cs
@@ -9,10 +9,20 @@ using Tenrec.Utils;
 namespace Tenrec.Generators.CS
 {
     /// <summary>
-    /// Creates unit-tests in XUnit framework for selected grasshopper script files containing Tenrec groups.
+    /// Produces code files automatically, generating test code based on Grasshopper files.
     /// </summary>
+    /// <remarks> 
+    /// This must be called once to produce the test files automatically, or every time you add or remove a <see cref="Group_UnitTest"/> or add or remove a file from the test folder.
+    /// </remarks>
     public class XUnitGenerator : IGenerator
     {
+        /// <summary>
+        /// Generate a code file containing all the <see cref="Group_UnitTest"/>-based tests present in Grasshopper files.
+        /// </summary>
+        /// <param name="ghTestFolders">The folder containing the Grasshopper files.</param>
+        /// <param name="outputFolder">The folder where to save the source code file.</param>
+        /// <param name="outputName">The name of the resulting code file.</param>
+        /// <returns>A log of the process.</returns>
         public string CreateAutoTestSourceFile
             (string[] ghTestFolders, string outputFolder, string outputName)
         {
--- Generators/IGenerator.cs
+++ Generators/IGenerator.cs
@@ -1,7 +1,20 @@
 ﻿namespace Tenrec.Generators
 {
+    /// <summary>
+    /// Produces code files automatically, generating test code based on Grasshopper files.
+    /// </summary>
+    /// <remarks> 
+    /// This must be called once to produce the test files automatically, or every time you add or remove a <see cref="Group_UnitTest"/> or add or remove a file from the test folder.
+    /// </remarks>
     public interface IGenerator
     {
+        /// <summary>
+        /// Generate a code file containing all the <see cref="Group_UnitTest"/>-based tests present in Grasshopper files.
+        /// </summary>
+        /// <param name="ghTestFolders">The folder containing the Grasshopper files.</param>
+        /// <param name="outputFolder">The folder where to save the source code file.</param>
+        /// <param name="outputName">The name of the resulting code file.</param>
+        /// <returns>A log of the process.</returns>
         string CreateAutoTestSourceFile
             (string[] ghTestFolders, string outputFolder, string outputName);
     }
